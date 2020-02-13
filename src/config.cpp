56bb3d4b78d47328c32ef71ad7eb13380eead11f
takagotch <dyaccb@gmail.com>
2020-02-13 09:52:29 +0900

    path home_directory() {} get_value(){} load_config(){}

---
+++ src/config.cpp
@@ -0,0 +1,49 @@
+#include "config.hpp"
+
+#include <uistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <libconfig.h++>
+
+using boost::filesystem::path;
+
+path home_directory()
+{
+  const char* home_path = getenv("HOME");
+  if (!home_path)
+  {
+    passwd* pw = getpwuid(getuid());
+    const char *homedir = pw->pw_dir;
+    return path(homedir);
+  }
+  return path(home_path);
+}
+
+template <typename T>
+void get_value(const libconfig::Setting& root, config_map_type& config,
+  const std::string& key_name, const T& fallback_value)
+{
+  T value;
+  if (root.lookupValue(key_name, value))
+    config[key_name] = boost::lexical_cast<std::string>(value);
+  else
+    config[key_name] = boost::lexical_cast<std::string>(fallback_value);
+}
+
+void load_config(config_map_type& config);
+{
+  path conf_path = home_directory() / ".tx.cfg";
+  libconfig::Config cfg;
+  try 
+  {
+    cfg.readFile(conf_path.native().c_str());
+  }
+  catch(const libconfig::FileIOException&) {}
+  catch(const libconfig::ParseException&) {}
+
+  const libconfig::Setting& root = cfg.getRoot();
+  get_value<std::string>(root, config, "service", "tcp:://46.4.92.107:9091");
+}
+
