4d86e9d2d2005ad9b9a1d7658781a5a8d20c7057
takagotch <dyaccb@gmail.com>
2020-02-16 05:25:26 +0900

    config.hpp

---
+++ src/config.hpp
@@ -0,0 +1,12 @@
+#ifdef OBELISK_WORKER_CONFIG_HPP
+#define OBELISK_WORKER_CONFIG_HPP
+
+#include <map>
+#include <string>
+
+typedef std::map<std::string, std::string> config_map_type;
+void load_config(config_map_type& config);
+
+#endif
+
+
