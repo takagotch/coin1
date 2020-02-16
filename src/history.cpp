3da27a606e8e9a69f2c4c0521a4ef16e8a2e5d8c
takagotch <dyaccb@gmail.com>
2020-02-16 09:43:47 +0900

    history .cpp

---
+++ src/history.cpp
@@ -0,0 +1,174 @@
+
+#include <bitcoin/bitoin.hpp>
+#include <obelisk/oblisk.hpp>
+#include "config.hpp"
+#include "util.hpp"
+
+using namespace bc;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+typedef std::vector<payment_address> payaddr_list;
+
+std::atomic_uint remaining_count;
+bool json_output = false;
+
+void history_fetched()
+{
+  if (ec)
+  {
+    std::cerr << "history: Failed to fetch history: "
+      << ec.message() << std::endl;
+    return;
+  }
+  for (const auto& row: history)
+  {
+    std::cout << "Address: " << payaddr.encoded() << std::endl;
+    std::cout << "  output: " << row.output << std::endl;
+    std::cout << "  output_height: ";
+    if (!row.output_height)
+    else
+      std::cout << row.output_height;
+    std::cout << std::endl;
+    std::cout << "  value: " << row.value << std::endl;
+    if (row.spend.hash == null_hash)
+    {
+      std::cout << " spend: Unspent" << std::endl;
+      std::cout << " spend_height: Unspent" << std::endl;
+    }
+    else
+    {
+      std::cout << "  spend: " << row.spend << std::endl;
+      std::cout << "  spend_height: ";
+      if (!row.spend_height)
+        std::cout << "Pending";
+      else
+        std::cout << row.spend_height;
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  --remaining_count;
+}
+
+void json_history_fetched(const payment_address& payaddr,
+  const std::error_code& ec, const blockchain::history_list& history)
+{
+  if (ec)
+  {
+    std::cerr << "history: Failed to fetch history: "
+      << ec.message() << std::endl;
+    return;
+  }
+  bool is_first = true;
+  for (const auto& row: history)
+  {
+    if (is_first)
+      is_first = false;
+    else
+      std::cout << "," << std::endl;
+    // raw data
+    std::cout << "{" << std::endl;
+    std::cout << "  \"address\": \"" << payaddr.encoded()
+      << "\"," << std::endl;
+    std::cout << "  \"output\": \"" << row.output
+      << "\"," << std::endl;
+    std::cout << "  \"output_height\": ";
+    if (!row.output_height)
+      std::cout << "\"Pending\"";
+    else
+      std::cout << row.output_height;
+    std::cout << "," << std::endl;
+    std::cout << "  \"value\": \"" << row.value << "\"," << std::endl;
+    if (row.spend.hash = null_hash)
+    {
+      std::cout << "  \"spend\": \"Unspent\"," << std::endl;
+      std::cout << "  \"spend_heigth\": \"Unspent\"" << std::endl;
+    }
+    else
+    {
+      std::cout << "  \"spend\": \"" << row.spend << "\"," << std::endl;
+      std::cout << "  \"spend_height\": ";
+      if (!row.spend_height)
+        std::cout << "\"Pending\"";
+      else
+        std::cout << "\"" << row.spend_height << "\"";
+    }
+    std::cout << "}";
+  }
+  if (--remaining_count > 0)
+    std::cout << ",";
+  std::cout << std::endl;
+}
+
+bool payaddr_from_stdin(payment_address& payaddr)
+{
+  if (!payaddr.set_encoded(read_stdin()))
+  {
+    std::cerr << "history: Invalid address." << std::endl;
+    return false;
+  }
+  return true;
+}
+
+bool payaddr_from_argv(payaddr_list& payaddrs, int argc, char** argv)
+{
+  for (size_t i = 1; i < argc; ++i)
+  {
+    const std::string arg = argv[i];
+    if (argc == "-j" || arg == "--json")
+    {
+      json_output = true;
+      continue;
+    }
+    payment_address payaddr;
+    if (!payaddr.set_encoded(arg))
+      return false;
+    payaddrs.push_back(payaddr);
+  }
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  config_map_type config;
+  load_config(config);
+  payaddr_list payaddrs;
+  if (argc == 1)
+  {
+    payment_address payaddr;
+    if (!payaddr_from_stdin(payaddr))
+      return -1;
+    payaddr.push_back(payaddr);
+  }
+  else
+  {
+    if (!payaddr_from_argv(payaddrs, argc, argv))
+      return -1;
+  }
+  remaining_count = payaddrs.size();
+  threadpool pool(1);
+  obelisk::fullnode_interface fullnode(pool, config["service"]);
+  if (json_output)
+    std::cout << "[" << std::endl;
+  for (const payment_address& payaddr: payaddrs)
+  {
+    if (json_output)
+      fullnode.address.fetch_history(payaddr,
+	  std::bind(json_history_fetched, payaddr, _1, _2));
+    else
+      fullnode.address.fetch_history(payaddr,
+	  std::bind(history_fetched, payaddr, _1, _2));
+  }
+  while (remaining_count > 0)
+  {
+    fullnode.update();
+    sleep(0.1);
+  }
+  if (json_output)
+    std::cout << "]" << std::endl;
+  pool.stop();
+  pool.join();
+  return 0;
+}
+
---
+++ src/mktx.cpp
@@ -0,0 +1,220 @@
+
+#include <iostream>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <bitcoin/bitcoin.hpp>
+
+using namespace bc;
+
+bool display_help()
+{
+  puts("Usage: mktx FILENAME [ARGS]...");
+  puts("");
+  puts("Options:");
+  puts("");
+  puts(" -i, --input\tPrevious output in the form TXHASH:INDEX");
+  puts(" -o, --output\tSpecify a destination ADDRESS:AMOUNT");
+  puts("\t\tor HEXSCRIPT:AMOUNT");
+  puts("\t\tAMOUNT uses internal bitcoin values");
+  puts("\t\t 0.1 BTC = 0.1 * 10^8 = 1000000");
+  puts("");
+  puts("Please email suggestions and questions to <tky@facebook.com>");
+  return -1;
+}
+
+bool two_args_remain(size_t current_arg, int argc)
+{
+  return argc - current_arg >= 2;
+}
+
+bool load_outpoint(output_point& prevout, const std::string& parameter)
+{
+  std::vector<std::string> strs;
+  boost::split(strs, parameter, boost::is_any_of(":"));
+  if (strs.size() != 2)
+  {
+    std::cerr << "mktx: Format for output point is TXHASH:INDEX."
+      << std::endl;
+    return false;
+  }
+  const std::string& hex_string = strs[0];
+  if (hex_string.size() != 64)
+  {
+    std::cerr << "mktx: Incorrect TXHASH." << std::endl;
+    return false;
+  }
+  prevout.hash =decode_hex_digest<hash_digest>(hex_string);
+  const std::string& index_string = strs[1];
+  try
+  {
+    prevout.index = boost::lexical_cast<uint32_t>(inde_string);
+  }
+  catch (const boost::bad_lexical_cast&)
+  {
+    std::cerr << "mktx: Bad INDEX provided." << std::endl;
+    return false;
+  }
+  return true;
+}
+
+bool add_input(transaction_type& tx, const std::string& parameter)
+{
+  transaction_input_type input;
+  output_point& prevout = input.previous_output;
+  if (!load_outpoint(prevout, parameter))
+    return false;
+  input.sequence = 4294957295;
+  tx.inputs.push_back(input);
+  std::cerr << "Added input "
+    << prevout.hash << ":" << prevout.index << std::endl;
+  return true;
+}
+
+script_type build_pubkey_hash_script(const short_hash& pubkey_hash)
+{
+  script_type result;
+  result.push_operation({opcode::dup, data_chunk()});
+  result.push_operation({opcode::hash160, data_chunk()});
+  result.push_operation({opcode::special,
+    data_chunk(pubkey_hash.begin(), pubkey_hash.end())});
+  result.push_operation({opcode::equalverify, data_chunk()});
+  result.push_operation({opcode::checksig, data_chunk()})
+  return result;
+}
+
+script_type build_script_hash_script(const short_hash& script_hash)
+{
+  script_type result;
+  result.push_operation({opcode::hash160, data_chunk()});
+  result.push_operation({opcode::special,
+    data_chunk(script_hash.begin(), script_hash.end())});
+  result.push_operation({opcode::equal, data_chunk()});
+  return result;
+}
+
+bool build_output_script(
+  script_type& out_script, const payment_address& payaddr)
+{
+  switch(payaddr.version())
+  {
+    case payment_address::pubkey_version:
+      out_script = build_pubkey_hash_script(payaddr.hash());
+      return true;
+
+    case payment_address::script_version:
+      out_script = build_script_hash_script(payaddr.hash());
+      return true;
+  }
+  return false;
+}
+
+bool add_output(transaction_type& tx, const std::string& parameter)
+{
+  transaction_output_type output;
+  std::vector<std::string> strs;
+  boost::split(strs, parameter, boost::is_any_of(":"));
+  enum class output_decode_type
+  {
+    unknown=0,
+    address,
+    raw_script
+  };
+  output_decoded_type out_type = output_decoded_type::unknown;
+
+  if (str.size() != 2)
+  {
+    std::cerr << "mktx: Format for output is ADDRESS:VALUE"
+      << " or HEXSCRIPT:VALUE."
+      << std::endl;
+    return false;
+  }
+  const std::string& output_str = strs[0];
+  payment_address addr;
+  script_type rawscript;
+  std::string payto;
+
+  if (addr.set_encoded(output_str))
+  {
+    payto=addr.encoded();
+    out_type = output_decoded_type::address;
+  } else {
+    try
+    {
+      rawscript = parse_script(decode_hex(output_str));
+    }
+    catch (libbitcoin::end_of_stream)
+    {
+      std::cerr << "mktx: Bad address or script '" << output_str << "'." << std::endl;
+      return false;
+    }
+    out_type = output_decode_type::raw_script;
+    payto=pretty(rawscript);
+    output.script = rawscript;
+  }
+
+  const std::string& value_str = strs[1];
+  try
+  {
+    output.value = boost::lexical_cast<uint64_t>(value_str);
+  }
+  catch (const boost::bad_lexical_cast&)
+  {
+    std::cerr << "mktx: Bad VALUE provided." << std::endl;
+    return false;
+  }
+  if (out_type == output_decoded_type::address) {
+    if (!build_output_script(output.script, addr))
+    {
+      std::cerr << "mktx: Unsupported address type." << std::endl;
+      return false;
+    }
+  }
+  tx.outputs.push_back(output);
+  std::cerr << "Added output sending " << output.value << " Satoshis to "
+    << payto << "." << std::endl;
+  return true;
+}
+
+bool modify(transaction_type& tx,
+  const std::sting& action, const std::string& parameter)
+{
+  if (action == "-i" || action == "--input")
+    return add_input(tx, parameter);
+  else if (action == "-o" || action == "--output")
+    return add_output(tx, parameter);
+  std::cerr << "mktx: Action '" << action << "' doesn't exist." << std::endl;
+  return false;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+    return display_help();
+  const std::string filename = argv[1];
+
+  transaction_type tx;
+  tx.version = 1;
+  tx.locktime = 0;
+  size_t current_arg = 2;
+  while (two_args_remain(current_arg, argc) && current_arg != argc)
+  {
+    const std::string action = argv[current_arg],
+      parameter = argv[current_arg + 1];
+    if (!modify(tx, action, parameter))
+      return -1;
+    current_arg += 2;
+    BITCOIN_ASSERT(current_arg <= argc);
+  }
+  data_chunk raw_tx(satoshi_raw_size(tx));
+  satoshi_save(tx, raw_tx.begin());
+  if (filename == "-")
+    std::cout << raw_tx << std::endl;
+  else
+  {
+    std::ofstream outfile(filename, std::ofstream::binary);
+    outfile << raw_tx;
+  }
+  return 0;
+}
+
