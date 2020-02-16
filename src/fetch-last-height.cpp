80768cd0cb63e0f058606b4505b2d05f9166930b
takagotch <dyaccb@gmail.com>
2020-02-17 03:22:22 +0900

    fetch-last-height.cpp

---
+++ src/fetch-last-height.cpp
@@ -0,0 +1,40 @@
+
+#include <bitcoin/bitcoin.hpp>
+#include <obelisk/obelisk.hpp>
+#include "config.hpp"
+#include "util.hpp"
+
+using namespace bc;
+
+bool stopped = false;
+
+void height_fetched(const std::error_code& ec, size_t height)
+{
+  if (ec)
+  {
+    std::cerr << "fetch-last-height: " << ec.message() << std::endl;
+    stopped = true;
+    return;
+  }
+  std::cout << height << std::endl;
+  stopped = true;
+}
+
+int main()
+{
+  config_map_type config;
+  load_config(config);
+  threadpool pool(1);
+  obelisk::fullnode_interface fullnode(pool, config["service"]);
+  fullnode.blockchain.fetch_last_height(height_fetched);
+  while (!stopped)
+  {
+    fullnode.update();
+    sleep(0.1);
+  }
+  pool.stop();
+  pool.join();
+  return 0;
+}
+
+
